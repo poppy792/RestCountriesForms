e86c7b8fb0eb313a8c77462d55480aca84611b67
student <student@DESKTOP-PG8244N>
2017-11-09 11:59:05 +0100

    second

--- WindowsFormsCountries/WindowsFormsCountries/Form1.Designer.cs
+++ WindowsFormsCountries/WindowsFormsCountries/Form1.Designer.cs
@@ -34,21 +34,43 @@
             this.comboBoxSort = new System.Windows.Forms.ComboBox();
             this.lblKontinent = new System.Windows.Forms.Label();
             this.lblSort = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Pregled = new System.Windows.Forms.TabPage();
+            this.Unos = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountries)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.Pregled.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridViewCountries
             // 
+            this.dataGridViewCountries.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridViewCountries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewCountries.Location = new System.Drawing.Point(225, 51);
+            this.dataGridViewCountries.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridViewCountries.Location = new System.Drawing.Point(203, 19);
             this.dataGridViewCountries.Name = "dataGridViewCountries";
-            this.dataGridViewCountries.Size = new System.Drawing.Size(695, 399);
+            this.dataGridViewCountries.Size = new System.Drawing.Size(643, 423);
             this.dataGridViewCountries.TabIndex = 0;
             // 
             // comboBoxRegion
             // 
             this.comboBoxRegion.FormattingEnabled = true;
-            this.comboBoxRegion.Location = new System.Drawing.Point(12, 94);
+            this.comboBoxRegion.Location = new System.Drawing.Point(19, 19);
             this.comboBoxRegion.Name = "comboBoxRegion";
             this.comboBoxRegion.Size = new System.Drawing.Size(178, 21);
             this.comboBoxRegion.TabIndex = 1;
@@ -57,7 +79,7 @@
             // comboBoxSort
             // 
             this.comboBoxSort.FormattingEnabled = true;
-            this.comboBoxSort.Location = new System.Drawing.Point(12, 160);
+            this.comboBoxSort.Location = new System.Drawing.Point(19, 59);
             this.comboBoxSort.Name = "comboBoxSort";
             this.comboBoxSort.Size = new System.Drawing.Size(178, 21);
             this.comboBoxSort.TabIndex = 2;
@@ -66,7 +88,7 @@
             // lblKontinent
             // 
             this.lblKontinent.AutoSize = true;
-            this.lblKontinent.Location = new System.Drawing.Point(9, 78);
+            this.lblKontinent.Location = new System.Drawing.Point(16, 3);
             this.lblKontinent.Name = "lblKontinent";
             this.lblKontinent.Size = new System.Drawing.Size(54, 13);
             this.lblKontinent.TabIndex = 3;
@@ -75,11 +97,103 @@
             // lblSort
             // 
             this.lblSort.AutoSize = true;
-            this.lblSort.Location = new System.Drawing.Point(9, 144);
+            this.lblSort.Location = new System.Drawing.Point(16, 43);
             this.lblSort.Name = "lblSort";
             this.lblSort.Size = new System.Drawing.Size(57, 13);
             this.lblSort.TabIndex = 4;
             this.lblSort.Text = "Sortiraj po:";
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "sCode";
+            this.Column1.HeaderText = "Kod";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "sName";
+            this.Column2.HeaderText = "Naziv";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "sCapital";
+            this.Column3.HeaderText = "Glavni grad";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "nPopulation";
+            this.Column4.HeaderText = "Stanovništvo";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "fArea";
+            this.Column5.HeaderText = "Površina";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "sRegion";
+            this.Column6.HeaderText = "Kontinent";
+            this.Column6.Name = "Column6";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Pregled);
+            this.tabControl1.Controls.Add(this.Unos);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(934, 495);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // Pregled
+            // 
+            this.Pregled.Controls.Add(this.dataGridViewCountries);
+            this.Pregled.Controls.Add(this.lblKontinent);
+            this.Pregled.Controls.Add(this.comboBoxRegion);
+            this.Pregled.Controls.Add(this.lblSort);
+            this.Pregled.Controls.Add(this.comboBoxSort);
+            this.Pregled.Location = new System.Drawing.Point(4, 22);
+            this.Pregled.Name = "Pregled";
+            this.Pregled.Padding = new System.Windows.Forms.Padding(3);
+            this.Pregled.Size = new System.Drawing.Size(926, 469);
+            this.Pregled.TabIndex = 0;
+            this.Pregled.Text = "Karta svijeta";
+            this.Pregled.UseVisualStyleBackColor = true;
+            // 
+            // Unos
+            // 
+            this.Unos.Location = new System.Drawing.Point(4, 22);
+            this.Unos.Name = "Unos";
+            this.Unos.Padding = new System.Windows.Forms.Padding(3);
+            this.Unos.Size = new System.Drawing.Size(926, 469);
+            this.Unos.TabIndex = 1;
+            this.Unos.Text = "Unos";
+            this.Unos.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(926, 469);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Karta svijeta";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(926, 469);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://www.openstreetmap.org", System.UriKind.Absolute);
             // 
             // Form1
             // 
@@ -88,17 +202,16 @@
             this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(932, 490);
-            this.Controls.Add(this.lblSort);
-            this.Controls.Add(this.lblKontinent);
-            this.Controls.Add(this.comboBoxSort);
-            this.Controls.Add(this.comboBoxRegion);
-            this.Controls.Add(this.dataGridViewCountries);
+            this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "REST Countires";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountries)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.Pregled.ResumeLayout(false);
+            this.Pregled.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -109,6 +222,17 @@
         private System.Windows.Forms.ComboBox comboBoxSort;
         private System.Windows.Forms.Label lblKontinent;
         private System.Windows.Forms.Label lblSort;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Pregled;
+        private System.Windows.Forms.TabPage Unos;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
